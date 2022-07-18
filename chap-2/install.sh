ca2588bba561da1859f3167f3eb95c8c2bee8f5d
vishalsingh17 <vishal170997@gmail.com>
2022-07-18 15:41:31 +0530

    new files added

---
+++ chap-2/install.sh
@@ -0,0 +1,18 @@
+# !/bin/bash
+# Script to install Linun system tools and basic python components
+# GENRAL LINUX
+apt-get update   #update the package index cache
+apt-get upgrade -y  #upgrade all packages installed
+
+#INTALL MINICONDA
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda.sh      #download the miniconda script
+bash Miniconda.sh -b     #install miniconda
+rm -rf Miniconda.sh	   #remove the miniconda script
+export PATH="/root/miniconda3/bin:$PATH"	   #set the path to the miniconda bin directory
+
+#INSTALL PYTHON LIBRARIES
+conda create -n myenv python=3.7	   #create a conda environment with python 3.6
+conda activate myenv	   #activate the conda environment
+conda update -y conda python     #update the conda and python environment
+conda install -y pandas      #install pandas
+conda install -y ipython      #install ipython shell
